988bd1f99fd5059644b4c0a3e535d2d7de8db688
Tom <pjoh1092@gmail.com>
2023-03-21 20:07:00 -0500

    Welcome to Java

---
+++ Hackerrank_Exercise/src/main/java/Java_Exercise/WelcomeToJave.java
@@ -0,0 +1,10 @@
+package Java_Exercise;
+import java.io.*;
+import java.util.*;
+public class WelcomeToJave {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
