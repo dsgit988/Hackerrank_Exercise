f4309738ac28a4f55e8a76b143e95b654dee5c7f
Tom Zhen <103967450+dsgit988@users.noreply.github.com>
2023-03-27 18:41:18 -0500

    Java Stdin and Stdout I

---
+++ Hackerrank_Exercise/src/main/java/Java_Exercise/JavaStdinandStdoutI.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        // Complete this line
+        System.out.println(c);
+        // Complete this line
+    }
+}
