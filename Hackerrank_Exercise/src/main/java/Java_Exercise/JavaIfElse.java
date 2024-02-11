0a8e5d2cc0982e16fdbb1583580bb3165e672e1c
Tom <pjoh1092@gmail.com>
2024-02-11 13:53:17 -0600

    java if else

---
+++ Hackerrank_Exercise/src/main/java/Java_Exercise/JavaIfElse.java
@@ -0,0 +1,26 @@
+package Java_Exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaIfElse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if (N % 2 != 0){
+            System.out.println("Weird");
+        }else {
+            if (N >= 2 && N <= 5){
+                System.out.println("Not Weird");
+            }else if (N >= 6 && N <= 20){
+                System.out.println("Weird");
+            }else if (N > 20){
+                System.out.println("Not Weird");
+            }
+        }
+
+        bufferedReader.close();
+    }
+}
